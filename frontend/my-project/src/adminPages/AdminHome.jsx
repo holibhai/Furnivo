870187719f0a4496a8d62b49f03c76ae6054a4dc
holibhai <146805057+holibhai@users.noreply.github.com>
2025-06-01 15:07:18 +0530

    admin home minor changea

--- frontend/my-project/src/adminPages/AdminHome.jsx
+++ frontend/my-project/src/adminPages/AdminHome.jsx
@@ -652,7 +652,7 @@ const AdminHome = () => {
                 </th>
                 <th scope="col" className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                   Actions
-                </th>
+                </th> 
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
