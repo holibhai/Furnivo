c12f836148765a4e133e3952ba3cca4cbd264ac0
holibhai <146805057+holibhai@users.noreply.github.com>
2025-06-05 01:31:09 +0530

    delivery  service Imple created

---
+++ backend/src/main/java/com/crudoperation/jw/service/servicesInterface/DeliveryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.crudoperation.jw.service.servicesInterface;
+
+import com.crudoperation.jw.dto.Response;
+import com.crudoperation.jw.model.Delivery;
+
+public interface DeliveryServiceImpl {
+    public Response addDeliveryType(Delivery delivery);
+}
