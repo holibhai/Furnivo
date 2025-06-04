d5fd2f9dc1240bfe1b97b9be1f3be00e07c67167
holibhai <146805057+holibhai@users.noreply.github.com>
2025-06-04 19:23:08 +0530

    minor changes

--- backend/src/main/java/com/crudoperation/jw/repo/OrderItemRepository.java
+++ backend/src/main/java/com/crudoperation/jw/repo/OrderItemRepository.java
@@ -1,7 +1,15 @@
 package com.crudoperation.jw.repo;
 
+import com.crudoperation.jw.model.Order;
 import com.crudoperation.jw.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+
+
+
+
 }
