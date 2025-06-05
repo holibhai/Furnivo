6ac79d34a42595613e0d2a21281078658d6ce327
holibhai <146805057+holibhai@users.noreply.github.com>
2025-06-06 00:59:28 +0530

    password dto created

---
+++ backend/src/main/java/com/crudoperation/jw/dto/Password.java
@@ -0,0 +1,27 @@
+package com.crudoperation.jw.dto;
+
+public class Password {
+    private String currentPassword;
+    private String newPassword;
+
+    public Password(String currentPassword, String newPassword) {
+        this.currentPassword = currentPassword;
+        this.newPassword = newPassword;
+    }
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
